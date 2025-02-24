72e7773371f6d509fb5b05c67a721b453260bb28
ahorenko <ahorenko@01-AHORENKOPC>
2025-02-24 17:54:41 +0600

    Надо переписать CRUD, для группы

---
+++ BLL/MR/StudentMR/Command/CreateStudent/CreateStudentCommand.cs
@@ -0,0 +1,15 @@
+﻿using DataMath.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.MR.StudentMR.Command.CreateStudent
+{
+    public class CreateStudentCommand : IRequest<Student>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ BLL/Repository/StudentRepository.cs
@@ -0,0 +1,50 @@
+﻿using DataMath;
+using DataMath.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace BLL.Repository
+{
+    public class StudentRepository(IMathContext ctx) : IGenericRepository<Student>
+    {
+        private readonly IMathContext _ctx = ctx;
+        public async Task AddAsync(Student entity, CancellationToken cancellationToken = default)
+        {
+            await _ctx.Students.AddAsync(entity, cancellationToken);
+            await _ctx.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(Student entity, CancellationToken cancellationToken = default)
+        {
+            _ctx.Students.Remove(entity);
+            await _ctx.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<IEnumerable<Student>> FindAsync(Expression<Func<Student, bool>> predicate, CancellationToken cancellationToken = default)
+        {
+            return await _ctx.Students.Where(predicate).ToListAsync(cancellationToken);
+        }
+
+        public async Task<IEnumerable<Student>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _ctx.Students.ToListAsync(cancellationToken);
+        }
+
+        public async Task<Student> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var student = await _ctx.Students.FindAsync(id, cancellationToken);
+            if (student is null)
+            {
+                throw new InvalidOperationException($"Student with {id} doesn't exist!");
+            }
+            return student;
+        }
+
+        public async Task UpdateAsync(Student entity, CancellationToken cancellationToken = default)
+        {
+            _ctx.Students.Update(entity);
+            await _ctx.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
